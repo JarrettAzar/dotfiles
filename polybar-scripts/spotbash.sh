ab940f21175d6359adf6a2fcb1cfcbf2a2d850e8
Jarrett Azar <30982357+JarrettAzar@users.noreply.github.com>
2019-06-19 22:45:39 -0400

    Add files via upload

---
+++ polybar-scripts/spotbash.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ps_out=`ps -ef | grep $1 | grep -v 'grep' | grep -v $0`
+result=$(echo $ps_out | grep "$1")
+if [[ "$result" != "" ]];then
+    echo ""
+else
+    echo "Not Running"
+fi
